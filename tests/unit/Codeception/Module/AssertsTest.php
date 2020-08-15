23d86d467a0a2a23a9a35e42dae925482c5ba6d8
Tavo Nieves J <ganieves@outlook.com>
2020-08-15 13:21:15 -0500

    Created AbstractAsserts class to have greater clarity of what the Asserts.php does and increase maintainability. Ordered tests according to their source.

--- tests/unit/Codeception/Module/AssertsTest.php
+++ tests/unit/Codeception/Module/AssertsTest.php
@@ -23,74 +23,154 @@ class AssertsTest extends TestCase
         $this->module = new Asserts($container);
     }
 
-    public function testAsserts()
+    public function testCodeceptionAsserts()
     {
-        $this->module->assertEquals(1, 1);
-        $this->module->assertContains(1, [1, 2]);
-        $this->module->assertSame(1, 1);
-        $this->module->assertNotSame(1, '1');
-        $this->module->assertRegExp('/^[\d]$/', '1');
+        $this->module->assertFileNotExists(__FILE__ . '.notExist');
+        // assertGreaterOrEquals
+        // assertIsEmpty
+        // assertLessOrEquals
         $this->module->assertNotRegExp('/^[a-z]$/', '1');
-        $this->module->assertMatchesRegularExpression('/^[\d]$/', '1');
+        $this->module->assertRegExp('/^[\d]$/', '1');
+        // assertThatItsNot
+    }
+
+    public function testPHPUnitAsserts()
+    {
+        $this->module->assertArrayHasKey('one', ['one' => 1, 'two' => 2]);
+        // assertArrayNotHasKey
+        // assertClassHasAttribute
+        // assertClassHasStaticAttribute
+        // assertClassNotHasAttribute
+        // assertClassNotHasStaticAttribute
+        $this->module->assertContains(1, [1, 2]);
+        // assertContainsEquals
+        // assertContainsOnly
+        // assertContainsOnlyInstancesOf
+        $this->module->assertCount(3, [1, 2, 3]);
+        // assertDirectoryDoesNotExist
+        // assertDirectoryExists
+        // assertDirectoryIsNotReadable
+        // assertDirectoryIsNotWritable
+        // assertDirectoryIsReadable
+        // assertDirectoryIsWritable
         $this->module->assertDoesNotMatchRegularExpression('/^[a-z]$/', '1');
-        $this->module->assertStringStartsWith('fo', 'foo');
-        $this->module->assertStringStartsNotWith('ba', 'foo');
-        $this->module->assertStringEndsWith('oo', 'foo');
-        $this->module->assertStringEndsNotWith('fo', 'foo');
         $this->module->assertEmpty([]);
-        $this->module->assertNotEmpty([1]);
-        $this->module->assertNull(null);
-        $this->module->assertNotNull('');
-        $this->module->assertNotNull(false);
-        $this->module->assertNotNull(0);
-        $this->module->assertTrue(true);
-        $this->module->assertNotTrue(false);
-        $this->module->assertNotTrue(null);
-        $this->module->assertNotTrue('foo');
+        $this->module->assertEquals(1, 1);
+        $this->module->assertEqualsCanonicalizing([3, 2, 1], [1, 2, 3]);
+        $this->module->assertEqualsIgnoringCase('foo', 'FOO');
+        $this->module->assertEqualsWithDelta(1.0, 1.01, 0.1);
         $this->module->assertFalse(false);
+        $this->module->assertFileDoesNotExist(__FILE__ . '.notExist');
+        // assertFileEquals
+        // assertFileEqualsCanonicalizing
+        // assertFileEqualsIgnoringCase
+        $this->module->assertFileExists(__FILE__);
+        // assertFileIsNotReadable
+        // assertFileIsNotWritable
+        // assertFileIsReadable
+        // assertFileIsWritable
+        // assertFileNotEquals
+        // assertFileNotEqualsCanonicalizing
+        // assertFileNotEqualsIgnoringCase
+        // assertFinite
+        // assertGreaterThan
+        // assertGreaterThanOrEqual
+        // assertInfinite
+        $this->module->assertInstanceOf('Exception', new Exception());
+        $this->module->assertIsArray([1, 2, 3]);
+        $this->module->assertIsBool(true);
+        $this->module->assertIsCallable(function() {});
+        // assertIsClosedResource
+        $this->module->assertIsFloat(1.2);
+        $this->module->assertIsInt(2);
+        // assertIsIterable
+        $this->module->assertIsNotArray(false);
+        $this->module->assertIsNotBool([1, 2, 3]);
+        $this->module->assertIsNotCallable('test');
+        // assertIsNotClosedResource
+        $this->module->assertIsNotFloat(false);
+        $this->module->assertIsNotInt(false);
+        // assertIsNotIterable
+        $this->module->assertIsNotNumeric(false);
+        $this->module->assertIsNotObject(false);
+        // assertIsNotReadable
+        $this->module->assertIsNotResource(false);
+        $this->module->assertIsNotScalar(function() {});
+        $this->module->assertIsNotString(false);
+        // assertIsNotWritable
+        $this->module->assertIsNumeric('12.34');
+        $this->module->assertIsObject(new stdClass());
+        // assertIsReadable
+        $this->module->assertIsResource(fopen(__FILE__, 'r'));
+        $this->module->assertIsScalar('test');
+        $this->module->assertIsString('test');
+        // assertIsWritable
+        // assertJson
+        // assertJsonFileEqualsJsonFile
+        // assertJsonFileNotEqualsJsonFile
+        // assertJsonStringEqualsJsonFile
+        // assertJsonStringEqualsJsonString
+        // assertJsonStringNotEqualsJsonFile
+        // assertJsonStringNotEqualsJsonString
+        // assertLessThan
+        // assertLessThanOrEqual
+        $this->module->assertMatchesRegularExpression('/^[\d]$/', '1');
+        // assertNan
+        // assertNotContains
+        // assertNotContainsEquals
+        // assertNotContainsOnly
+        // assertNotCount
+        $this->module->assertNotEmpty([1]);
+        // assertNotEquals
+        $this->module->assertNotEqualsCanonicalizing([3, 2, 1], [2, 3, 0, 1]);
+        $this->module->assertNotEqualsIgnoringCase('foo', 'BAR');
+        $this->module->assertNotEqualsWithDelta(1.0, 1.5, 0.1);
         $this->module->assertNotFalse(true);
         $this->module->assertNotFalse(null);
         $this->module->assertNotFalse('foo');
-        $this->module->assertFileExists(__FILE__);
-        $this->module->assertFileNotExists(__FILE__ . '.notExist');
-        $this->module->assertFileDoesNotExist(__FILE__ . '.notExist');
-        $this->module->assertInstanceOf('Exception', new Exception());
-        $this->module->assertArrayHasKey('one', ['one' => 1, 'two' => 2]);
-        $this->module->assertCount(3, [1, 2, 3]);
-
+        // assertNotInstanceOf
+        $this->module->assertNotNull('');
+        $this->module->assertNotNull(false);
+        $this->module->assertNotNull(0);
+        $this->module->assertNotSame(1, '1');
+        // assertNotSameSize
+        $this->module->assertNotTrue(false);
+        $this->module->assertNotTrue(null);
+        $this->module->assertNotTrue('foo');
+        $this->module->assertNull(null);
+        // assertObjectHasAttribute
+        // assertObjectNotHasAttribute
+        $this->module->assertSame(1, 1);
+        // assertSameSize
         $this->module->assertStringContainsString('bar', 'foobar');
-        $this->module->assertStringContainsStringignoringCase('bar', 'FooBar');
+        $this->module->assertStringContainsStringIgnoringCase('bar', 'FooBar');
+        $this->module->assertStringEndsNotWith('fo', 'foo');
+        $this->module->assertStringEndsWith('oo', 'foo');
+        // assertStringEqualsFile
+        // assertStringEqualsFileCanonicalizing
+        // assertStringEqualsFileIgnoringCase
+        // assertStringMatchesFormat
+        // assertStringMatchesFormatFile
         $this->module->assertStringNotContainsString('baz', 'foobar');
-        $this->module->assertStringNotContainsStringignoringCase('baz', 'FooBar');
-
-        $this->module->assertIsArray([1, 2, 3]);
-        $this->module->assertIsBool(true);
-        $this->module->assertIsFloat(1.2);
-        $this->module->assertIsInt(2);
-        $this->module->assertIsNumeric('12.34');
-        $this->module->assertIsObject(new stdClass());
-        $this->module->assertIsResource(fopen(__FILE__, 'r'));
-        $this->module->assertIsString('test');
-        $this->module->assertIsScalar('test');
-        $this->module->assertIsCallable(function() {});
-
-        $this->module->assertIsNotArray(false);
-        $this->module->assertIsNotBool([1, 2, 3]);
-        $this->module->assertIsNotFloat(false);
-        $this->module->assertIsNotInt(false);
-        $this->module->assertIsNotNumeric(false);
-        $this->module->assertIsNotObject(false);
-        $this->module->assertIsNotResource(false);
-        $this->module->assertIsNotString(false);
-        $this->module->assertIsNotScalar(function() {});
-        $this->module->assertIsNotCallable('test');
-
-        $this->module->assertEqualsCanonicalizing([3, 2, 1], [1, 2, 3]);
-        $this->module->assertNotEqualsCanonicalizing([3, 2, 1], [2, 3, 0, 1]);
-        $this->module->assertEqualsIgnoringCase('foo', 'FOO');
-        $this->module->assertNotEqualsIgnoringCase('foo', 'BAR');
-        $this->module->assertEqualsWithDelta(1.0, 1.01, 0.1);
-        $this->module->assertNotEqualsWithDelta(1.0, 1.5, 0.1);
+        $this->module->assertStringNotContainsStringIgnoringCase('baz', 'FooBar');
+        // assertStringNotEqualsFile
+        // assertStringNotEqualsFileCanonicalizing
+        // assertStringNotEqualsFileIgnoringCase
+        // assertStringNotMatchesFormat
+        // assertStringNotMatchesFormatFile
+        $this->module->assertStringStartsNotWith('ba', 'foo');
+        $this->module->assertStringStartsWith('fo', 'foo');
+        // assertThat
+        $this->module->assertTrue(true);
+        // assertXmlFileEqualsXmlFile
+        // assertXmlFileNotEqualsXmlFile
+        // assertXmlStringEqualsXmlFile
+        // assertXmlStringEqualsXmlString
+        // assertXmlStringNotEqualsXmlFile
+        // assertXmlStringNotEqualsXmlString
+        // fail
+        // markTestIncomplete
+        // markTestSkipped
     }
 
     public function testExceptions()
